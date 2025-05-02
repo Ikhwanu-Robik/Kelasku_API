b8d170c677b2e7237c049ad2fc4a7e89cf7d627d
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-05-02 11:15:12 +0700

    feat: rate limiting for api routes

--- routes/api.php
+++ routes/api.php
@@ -10,30 +10,37 @@ use App\Http\Middleware\EnsureUserIsAdmin;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AdminLoginController;
 
-Route::middleware('guest')->group(function () {
-    Route::post('/register', RegisterController::class);
-    Route::post('/login', LoginController::class);
+Route::middleware(['throttle:api'])->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::post('/register', RegisterController::class);
+        Route::post('/login', LoginController::class);
 
-    Route::get('/schools', [SchoolController::class, 'index']);
-    Route::get('/schools/{school}', [SchoolController::class, 'show']);
+        Route::get('/schools', [SchoolController::class, 'index']);
+        Route::get('/schools/{school}', [SchoolController::class, 'show']);
 
-    Route::post('/admin/login', AdminLoginController::class);
-});
+        Route::post('/admin/login', AdminLoginController::class);
+    });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('users', UserController::class)->except('store', 'destroy');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('users', UserController::class)->except('store', 'destroy');
 
-    Route::put('/users/{user}/password', [UserController::class, 'updatePassword']);
+        Route::put('/users/{user}/password', [UserController::class, 'updatePassword']);
 
-    Route::post('/logout', LogoutController::class);
+        Route::post('/logout', LogoutController::class);
 
-    Route::post('/users/{user}/colek', ColekController::class);
-});
+        Route::post('/users/{user}/colek', ColekController::class);
+    });
 
-Route::middleware(['auth:sanctum', EnsureUserIsAdmin::class])->group(function () {
-    Route::post('/schools', [SchoolController::class, 'store']);
-    Route::put('/schools/{school}/', [SchoolController::class, 'update']);
-    Route::delete('/schools/{school}', [SchoolController::class, 'destroy']);
-    Route::put('/admin/users/{user}', [UserController::class, 'adminUpdate']);
-    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    Route::middleware(['auth:sanctum', EnsureUserIsAdmin::class])->group(function () {
+        Route::post('/schools', [SchoolController::class, 'store']);
+        Route::put('/schools/{school}/', [SchoolController::class, 'update']);
+        Route::delete('/schools/{school}', [SchoolController::class, 'destroy']);
+        Route::put('/admin/users/{user}', [UserController::class, 'adminUpdate']);
+        Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    });
+
+    Route::get('/testthrottle', function () {
+        sleep(2);
+        return "Slept for 2 seconds lol";
+    });
 });
